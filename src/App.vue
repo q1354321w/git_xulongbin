ca3d5b2e123b8f3cda61ef30b55e59ae176ddd6c
李倩云 <liqianyun@liqianyundeMacBook-Pro.local>
2021-07-01 21:52:05 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+    <MyBlog v-for="blog in blogs" :key="blog.id" :title="blog.title"></MyBlog>
+    </div>
+    
+    
+</template>
+<script type="text/ecmascript-6">
+import MyBlog from "./components/MyBlog";
+export default {
+    data(){
+        return{
+            blogs:[
+                {id:1,title:'one'},
+                {id:2,title:'twoo'},
+                {id:3,title:'three'}
+            ]
+        }
+    },
+    components:{
+        MyBlog,
+    }
+}
+</script>
+<style scoped>
+
+</style>
+
---
+++ src/components/MyBlog.vue
@@ -0,0 +1,21 @@
+<template>
+    <h2>
+        文章标题是{{title}}
+        <MyButton :title="title"></MyButton>
+    </h2>
+
+    
+</template>
+<script type="text/ecmascript-6">
+import MyButton from "./MyButton";
+export default {
+    props:['title'],
+    components:{
+        MyButton
+    }
+}
+</script>
+<style scoped>
+
+</style>
+
